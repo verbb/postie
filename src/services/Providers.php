1adfd0f7bf3b3bad9608897698eb36b5fb43df5f
Josh Crawford <joshua@sgroup.com.au>
2020-12-02 22:29:08 +1100

    Revert "Remove Colissimo - no API for rates"
    This reverts commit 2c48a3686056c152d04d3e2a728973852743f425.

--- src/services/Providers.php
+++ src/services/Providers.php
@@ -8,6 +8,7 @@ use verbb\postie\events\RegisterProviderTypesEvent;
 use verbb\postie\providers\AustraliaPost;
 use verbb\postie\providers\Bring;
 use verbb\postie\providers\CanadaPost;
+use verbb\postie\providers\Colissimo;
 use verbb\postie\providers\DHLExpress;
 use verbb\postie\providers\Fastway;
 use verbb\postie\providers\FedEx;
@@ -72,6 +73,7 @@ class Providers extends Component
             AustraliaPost::class,
             Bring::class,
             CanadaPost::class,
+            Colissimo::class,
             DHLExpress::class,
             Fastway::class,
             FedEx::class,
