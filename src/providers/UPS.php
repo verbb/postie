8168f7dcdbbc99fc0f7de94bdb336d98708063fa
Josh Crawford <joshua@sgroup.com.au>
2024-09-28 09:43:12 +1000

    Fix UPS phone number mapping not casting values as strings

--- src/providers/UPS.php
+++ src/providers/UPS.php
@@ -223,11 +223,11 @@ class UPS extends Provider
         if ($this->phoneField) {
             try {
                 $cart = Commerce::getInstance()->getCarts()->getCart();
-                $phoneValue = $cart->{$this->phoneField};
+                $phoneValue = (string)$cart->getFieldValue($this->phoneField);
 
                 if ($phoneValue) {
                     $payload = $event->getRequest()->getPayload();
-                    $payload['json']['RateRequest']['Shipment']['ShipTo']['Phone']['Number'] = $phoneValue;;
+                    $payload['json']['RateRequest']['Shipment']['ShipTo']['Phone']['Number'] = $phoneValue;
 
                     $event->getRequest()->setPayload($payload);
                 }
